522f54f7500cc669f0335fae870edf40a17b72d0
navalogy <team.navalogy@gmail.com>
2022-04-21 02:21:16 +0530

    reg

--- src/reg.php
+++ src/reg.php
@@ -80,8 +80,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: ../401.html");
             exit();
         }
-    }
-    elseif (strcmp($req, "essay") == 0) {
+    } elseif (strcmp($req, "essay") == 0) {
         $email = $_POST['reg-email'];
         $name = $_POST['reg-name'];
         $roll = $_POST['reg-roll'];
@@ -91,6 +90,98 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if (strcasecmp($temp, $roll) == 0) {
             $sql = "INSERT INTO `essay` (`email`, `name`, `rollno`, `dept`, `timestamp`) VALUES ('$email', '$name', '$roll', '$dept', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html");
+                exit();
+            }
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html");
+            exit();
+        }
+    } elseif (strcmp($req, "bgmi") == 0) {
+        $team = $_POST['reg-team'];
+        $temail = $_POST['reg-email'];
+        $tname = $_POST['reg-name'];
+        $troll = $_POST['reg-roll'];
+        $tdept = $_POST['reg-dept'];
+        $m1name = $_POST['reg-m1name'];
+        $m1roll = $_POST['reg-m1roll'];
+        $m2name = $_POST['reg-m2name'];
+        $m2roll = $_POST['reg-m2roll'];
+        $m3name = $_POST['reg-m3name'];
+        $m3roll = $_POST['reg-m3roll'];
+
+        $temp = substr($temail, 0, 8);
+
+        if (strcasecmp($temp, $troll) == 0) {
+            $sql = "INSERT INTO `bgmi` (`teamname`, `temail`, `tname`, `trollno`, `tdept`, `m1name`, `m1rollno`, `m2name`, `m2rollno`, `m3name`, `m3rollno`, `timestamp`) VALUES ('$team', '$temail', '$tname', '$troll', '$tdept', '$m1name', '$m1roll', '$m2name', '$m2roll', '$m3name', '$m3roll', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html?err=1");
+                exit();
+            }
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html?err=2");
+            exit();
+        }
+    } elseif (strcmp($req, "valo") == 0) {
+        $team = $_POST['reg-team'];
+        $temail = $_POST['reg-email'];
+        $tname = $_POST['reg-name'];
+        $troll = $_POST['reg-roll'];
+        $tdept = $_POST['reg-dept'];
+        $m1name = $_POST['reg-m1name'];
+        $m1roll = $_POST['reg-m1roll'];
+        $m2name = $_POST['reg-m2name'];
+        $m2roll = $_POST['reg-m2roll'];
+        $m3name = $_POST['reg-m3name'];
+        $m3roll = $_POST['reg-m3roll'];
+        $m4name = $_POST['reg-m4name'];
+        $m4roll = $_POST['reg-m4roll'];
+
+        $temp = substr($temail, 0, 8);
+
+        if (strcasecmp($temp, $troll) == 0) {
+            $sql = "INSERT INTO `valo` (`teamname`, `temail`, `tname`, `trollno`, `tdept`, `m1name`, `m1rollno`, `m2name`, `m2rollno`, `m3name`, `m3rollno`, `m4name`, `m4rollno`, `timestamp`) VALUES ('$team', '$temail', '$tname', '$troll', '$tdept', '$m1name', '$m1roll', '$m2name', '$m2roll', '$m3name', '$m3roll', '$m4name', '$m4roll', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html?err=1");
+                exit();
+            }
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html?err=2");
+            exit();
+        }
+    } elseif (strcmp($req, "iothon") == 0) {
+        $team = $_POST['reg-team'];
+        $temail = $_POST['reg-email'];
+        $tname = $_POST['reg-name'];
+        $troll = $_POST['reg-roll'];
+        $tdept = $_POST['reg-dept'];
+
+        $temp = substr($temail, 0, 8);
+
+        if (strcasecmp($temp, $troll) == 0) {
+            $sql = "INSERT INTO `iothon` (`teamname`, `temail`, `tname`, `trollno`, `tdept`, `timestamp`) VALUES ('$team', '$temail', '$tname', '$troll', '$tdept', current_timestamp());";
             $res = mysqli_query($con, $sql);
             if ($res) {
                 echo 'Registered';

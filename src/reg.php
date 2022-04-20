df021bf9cf631178bcd67f120163d1c69b984b36
navalogy <team.navalogy@gmail.com>
2022-04-21 01:18:03 +0530

    reg

--- src/reg.php
+++ src/reg.php
@@ -20,14 +20,88 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo 'Registered';
                 header("Location: ../200.html");
                 exit();
-            } 
-            else {
+            } else {
                 echo 'Not Registered';
                 header("Location: ../401.html");
                 exit();
             }
-        } 
-        else {
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html");
+            exit();
+        }
+    } elseif (strcmp($req, "suresh") == 0) {
+        $email = $_POST['reg-email'];
+        $name = $_POST['reg-name'];
+        $roll = $_POST['reg-roll'];
+        $dept = $_POST['reg-dept'];
+
+        $temp = substr($email, 0, 8);
+
+        if (strcasecmp($temp, $roll) == 0) {
+            $sql = "INSERT INTO `sureshv` (`email`, `name`, `rollno`, `dept`, `timestamp`) VALUES ('$email', '$name', '$roll', '$dept', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html");
+                exit();
+            }
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html");
+            exit();
+        }
+    } elseif (strcmp($req, "fifa") == 0) {
+        $email = $_POST['reg-email'];
+        $name = $_POST['reg-name'];
+        $roll = $_POST['reg-roll'];
+        $dept = $_POST['reg-dept'];
+
+        $temp = substr($email, 0, 8);
+
+        if (strcasecmp($temp, $roll) == 0) {
+            $sql = "INSERT INTO `fifa` (`email`, `name`, `rollno`, `dept`, `timestamp`) VALUES ('$email', '$name', '$roll', '$dept', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html");
+                exit();
+            }
+        } else {
+            echo 'Not Registered';
+            header("Location: ../401.html");
+            exit();
+        }
+    }
+    elseif (strcmp($req, "essay") == 0) {
+        $email = $_POST['reg-email'];
+        $name = $_POST['reg-name'];
+        $roll = $_POST['reg-roll'];
+        $dept = $_POST['reg-dept'];
+
+        $temp = substr($email, 0, 8);
+
+        if (strcasecmp($temp, $roll) == 0) {
+            $sql = "INSERT INTO `essay` (`email`, `name`, `rollno`, `dept`, `timestamp`) VALUES ('$email', '$name', '$roll', '$dept', current_timestamp());";
+            $res = mysqli_query($con, $sql);
+            if ($res) {
+                echo 'Registered';
+                header("Location: ../200.html");
+                exit();
+            } else {
+                echo 'Not Registered';
+                header("Location: ../401.html");
+                exit();
+            }
+        } else {
             echo 'Not Registered';
             header("Location: ../401.html");
             exit();
